12437646c8587bcac9e5b6c637813935c89d7d14
laeba <laeba2704@gmail.com>
2024-06-26 09:39:06 +0530

    dasdd

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import RestrauntMenu from "./pages/RestrauntMenu";
 import Cart from "./pages/Cart";
 import Navbar from "./Component/Navbar";
 
 function App() {
-  
-
   return (
     <div>
-    
-<BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/restaurants/:id" element={<RestrauntMenu/>} />
-        <Route path="/cart" element={<Cart/>} />
-      </Routes>
-    </BrowserRouter>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" component={Home} />
+          <Route path="/restaurants/:id" component={RestrauntMenu} />
+          <Route path="/cart" component={Cart} />
+        </Routes>
+      </Router>
     </div>
   );
 }
